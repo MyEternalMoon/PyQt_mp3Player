a0a2c02a249c06742eca17c8a0bc3bc26aa65f7e
DAshes <564035745@qq.com>
2018-08-24 18:05:55 +0800

    8/24 4.常规修正

--- Go_.py
+++ Go_.py
@@ -2,7 +2,6 @@ import sys
 from eyed3 import mp3
 from PyQt5 import QtWidgets,QtCore, Qt
 from widgets.MainWindow import PlayerMainWindow
-from ui.loading import Ui_Form
 import time
 
 
@@ -13,27 +12,27 @@ def main():
     sys.exit(app.exec_())
 
 
-class Loading(QtWidgets.QWidget, Ui_Form):
-    def __init__(self, parent=None):
-        super(Loading, self).__init__()
-        self.trueparent = parent
-        self.setupUi(self)
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.processing)
-        self.timer.setInterval(100)
-        self.timer.start()
-        self.setWindowFlags(Qt.Qt.FramelessWindowHint)
-        self.setAttribute(Qt.Qt.WA_TranslucentBackground)
-        self.v = 0
-
-    def processing(self):
-        self.v += 100
-        self.progressBar.setValue(self.v / 25)
-        if self.v == 1500:
-            self.show_main()
-
-    def show_main(self):
-        self.hide()
+# class Loading(QtWidgets.QWidget, Ui_Form):
+#     def __init__(self, parent=None):
+#         super(Loading, self).__init__()
+#         self.trueparent = parent
+#         self.setupUi(self)
+#         self.timer = QtCore.QTimer()
+#         self.timer.timeout.connect(self.processing)
+#         self.timer.setInterval(100)
+#         self.timer.start()
+#         self.setWindowFlags(Qt.Qt.FramelessWindowHint)
+#         self.setAttribute(Qt.Qt.WA_TranslucentBackground)
+#         self.v = 0
+#
+#     def processing(self):
+#         self.v += 100
+#         self.progressBar.setValue(self.v / 25)
+#         if self.v == 1500:
+#             self.show_main()
+#
+#     def show_main(self):
+#         self.hide()
 
 
 if __name__ == "__main__":
